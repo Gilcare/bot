734985203f7608f98da731895639afe156f3af87
Gilcare <gilcare.com@gmail.com>
2026-01-17 09:41:41 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import torch
+from huggingface_hub import login
 from transformers import pipeline, TextIteratorStreamer
 from threading import Thread
 
 st.markdown('<h1 style="color:#FF46A2; text-align: center;">PeriodIQ✨</h1>', unsafe_allow_html=True)
 st.divider()   
 
-HUGGINGFACE_API_KEY = st.secrets.huggingface_api_key
+HF_TOKEN = st.secrets.huggingface_api_key
 
 
 pipe = pipeline("image-text-to-text", model="google/medgemma-1.5-4b-it")
