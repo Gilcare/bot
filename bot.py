1b7b852f8ff6dc6cb2a4d9645f4dd1d133063150
Gilcare <gilcare.com@gmail.com>
2026-01-17 09:44:41 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import torch
 from huggingface_hub import login
-from transformers import pipeline, TextIteratorStreamer
+from transformers import pipeline
 from threading import Thread
 
 st.markdown('<h1 style="color:#FF46A2; text-align: center;">PeriodIQ✨</h1>', unsafe_allow_html=True)
