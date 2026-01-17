983715b7a04beb9f5b0027422bdaa30f1a48dfc0
Gilcare <gilcare.com@gmail.com>
2026-01-17 10:05:33 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import torch
 from huggingface_hub import login
-from transformers import AutoProcessor, AutoModelForImageTextToText
+from transformers import pipeline
+#from transformers import AutoProcessor, AutoModelForImageTextToText
 from threading import Thread
 
 st.markdown('<h1 style="color:#FF46A2; text-align: center;">PeriodIQ✨</h1>', unsafe_allow_html=True)
@@ -12,8 +13,8 @@ HF_TOKEN = st.secrets.HF_TOKEN
 
 
 
-processor = AutoProcessor.from_pretrained("google/medgemma-1.5-4b-it")
-model = AutoModelForImageTextToText.from_pretrained("google/medgemma-1.5-4b-it")
+
+pipe = pipeline("image-text-to-text", model="Qwen/Qwen3-VL-2B-Instruct")
 messages = [
     {
         "role": "user",
@@ -23,13 +24,4 @@ messages = [
         ]
     },
 ]
-inputs = processor.apply_chat_template(
-	messages,
-	add_generation_prompt=True,
-	tokenize=True,
-	return_dict=True,
-	return_tensors="pt",
-).to(model.device)
-
-outputs = model.generate(**inputs, max_new_tokens=40)
-print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
+pipe(text=messages)
