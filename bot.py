2e8b937409d9e9445984b57b3c8e5aeb33800ccb
Gilcare <gilcare.com@gmail.com>
2026-01-17 09:55:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,35 @@
 import streamlit as st
 import torch
 from huggingface_hub import login
-from transformers import pipeline
+from transformers import AutoProcessor, AutoModelForImageTextToText
 from threading import Thread
 
 st.markdown('<h1 style="color:#FF46A2; text-align: center;">PeriodIQ✨</h1>', unsafe_allow_html=True)
 st.divider()   
 
 HF_TOKEN = st.secrets.HF_TOKEN
-added_photo = st.file_uploader("Add Image",type = ["jpg", "jpeg", "png"])
 
-pipe = pipeline("image-to-text", model="google/medgemma-1.5-4b-it")
+
+
+
+processor = AutoProcessor.from_pretrained("google/medgemma-1.5-4b-it")
+model = AutoModelForImageTextToText.from_pretrained("google/medgemma-1.5-4b-it")
 messages = [
     {
         "role": "user",
         "content": [
-            {"type": "image", "image": added_photo},
-            {"type": "text", "text": "What is the graph saying?"}
+            {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+            {"type": "text", "text": "What animal is on the candy?"}
         ]
     },
 ]
-pipe(text=messages)
+inputs = processor.apply_chat_template(
+	messages,
+	add_generation_prompt=True,
+	tokenize=True,
+	return_dict=True,
+	return_tensors="pt",
+).to(model.device)
+
+outputs = model.generate(**inputs, max_new_tokens=40)
+print(processor.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
