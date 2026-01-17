04dc9dacdf3e2db615337598aa7a905d71d72e58
Gilcare <gilcare.com@gmail.com>
2026-01-17 09:33:29 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,18 @@ st.markdown('<h1 style="color:#FF46A2; text-align: center;">PeriodIQ✨</h1>', u
 st.divider()   
 
 
-hf auth login = st.secretshuggingface_api_key
+hf auth login = st.secrets.huggingface_api_key
+
+
+pipe = pipeline("image-text-to-text", model="google/medgemma-1.5-4b-it")
+added_photo = st.file_uploader("Add Image",type = ["jpg", "jpeg", "png"])
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "image": added_photo},
+            {"type": "text", "text": "What is the graph saying?"}
+        ]
+    },
+]
+pipe(text=messages)
